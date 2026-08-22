929fc27341d6d77deaf7fa87f54ad269be1c510b
gxenzy <gxenzy@users.noreply.github.com>
2026-08-22 23:00:48 +0800

    fix(mixin): use Screen.init() widget injection for AdminStatsOverlay

--- common/src/main/java/com/lunazstudios/virtualloot/mixin/cobblebase/CobblebaseAdminScreenMixin.java
+++ common/src/main/java/com/lunazstudios/virtualloot/mixin/cobblebase/CobblebaseAdminScreenMixin.java
@@ -1,39 +1,26 @@
 package com.lunazstudios.virtualloot.mixin.cobblebase;
 
-import com.lunazstudios.virtualloot.client.cobblebase.AdminStatsOverlay;
-import net.minecraft.client.gui.GuiGraphics;
+import com.lunazstudios.virtualloot.client.cobblebase.AdminStatsOverlayWidget;
+import net.minecraft.client.gui.components.Renderable;
+import net.minecraft.client.gui.components.events.GuiEventListener;
+import net.minecraft.client.gui.narration.NarratableEntry;
 import net.minecraft.client.gui.screens.Screen;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Screen.class)
 public abstract class CobblebaseAdminScreenMixin {
 
-    @Inject(method = "render", at = @At("TAIL"))
-    private void virtualloot$onAdminScreenRender(GuiGraphics context, int mouseX, int mouseY, float delta, CallbackInfo ci) {
-        if (this.getClass().getName().contains("AdminScreen")) {
-            AdminStatsOverlay.render(context, this, mouseX, mouseY, delta);
-        }
-    }
+    @Shadow
+    protected abstract <T extends GuiEventListener & Renderable & NarratableEntry> T addRenderableWidget(T widget);
 
-    @Inject(method = "mouseClicked", at = @At("HEAD"), cancellable = true)
-    private void virtualloot$onAdminScreenMouseClicked(double mouseX, double mouseY, int button, CallbackInfoReturnable<Boolean> cir) {
+    @Inject(method = "init()V", at = @At("TAIL"))
+    private void virtualloot$onScreenInit(CallbackInfo ci) {
         if (this.getClass().getName().contains("AdminScreen")) {
-            if (AdminStatsOverlay.mouseClicked(this, mouseX, mouseY, button)) {
-                cir.setReturnValue(true);
-            }
-        }
-    }
-
-    @Inject(method = "mouseScrolled", at = @At("HEAD"), cancellable = true)
-    private void virtualloot$onAdminScreenMouseScrolled(double mouseX, double mouseY, double scrollX, double scrollY, CallbackInfoReturnable<Boolean> cir) {
-        if (this.getClass().getName().contains("AdminScreen")) {
-            if (AdminStatsOverlay.mouseScrolled(this, mouseX, mouseY, scrollY)) {
-                cir.setReturnValue(true);
-            }
+            this.addRenderableWidget(new AdminStatsOverlayWidget((Screen) (Object) this));
         }
     }
 }
